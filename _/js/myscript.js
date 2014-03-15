ff504fe67405cb461a7734c98e6ec07a81df62f5
Benoit Cotte <benoit.cotte@essec.edu>
2014-03-15 16:03:10 +0100

    Installed Bootstrap template

---
+++ _/js/myscript.js
@@ -0,0 +1,2 @@
+
+//# sourceMappingURL=./myscript.map
--- index.php
+++ index.php
@@ -1,11 +1,16 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>OuiSkin</title>
-</head>
-
-<body>
-Hello, world
-</body>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap 101 Template</title>
+    <link href="_/css/bootstrap.css" rel="stylesheet">
+    <link href="_/css/mystyles.css" rel="stylesheet">
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+    <script src="_/js/bootstrap.js"></script>
+    <script src="_/js/myscript.js"></script>
+  </body>
 </html>
