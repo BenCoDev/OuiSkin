2bbfa0039763095e085890fc5db637e34f398cd8
Benoit Cotte <benoit.cotte@essec.edu>
2014-03-21 13:23:05 +0100

    basic scaffold of website complete

--- blog.php
+++ blog.php
@@ -4,12 +4,12 @@
       <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>OuiSkin -- Home</title>
+        <title>OuiSkin -- Blog</title>
         <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
         <link href="_/css/bootstrap.css" rel="stylesheet">
         <link href="_/css/mystyles.css" rel="stylesheet">
     </head>
-    <body id = "home">
+    <body id = "blog">
         <section class = "container">
             <div class="content row">
                 <section class = "main col col-lg-8">
