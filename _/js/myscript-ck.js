000aaf05b93dbd20bd430d1f274f701cd33649ec
Benoit Cotte <benoit.cotte@essec.edu>
2014-03-21 11:44:36 +0100

    Modified index

---
+++ _/js/myscript-ck.js
@@ -0,0 +1,2 @@
+
+//# sourceMappingURL=./myscript-ck.map
--- index.php
+++ index.php
@@ -1,18 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-      <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>OuiSkin -- Home</title>
-        <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
-        <link href="_/css/bootstrap.css" rel="stylesheet">
-        <link href="_/css/mystyles.css" rel="stylesheet">
-    </head>
-    <body id = "home">
-        <section class = "container">
-        </section>
-        <script src="_/js/bootstrap.js"></script>
-        <script src="_/js/myscript.js"></script>
-    </body>
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>OuiSkin</title>
+</head>
+
+<body>
+Hello, world
+</body>
 </html>
