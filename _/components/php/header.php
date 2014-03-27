7de81cbcc7d1e76004f40f1fe21088dc55c9afa8
Benoit Cotte <benoit.cotte@essec.edu>
2014-03-27 09:47:23 +0100

    back on track

---
+++ _/components/php/header.php
@@ -0,0 +1,18 @@
+<div class="content row">
+    <div class="col-lg-12">
+        <header class="clearfix">
+            <section id="branding">
+                <a href="index.php"><img src="images/misc/logo-OuiSkin.png" alt="logo for OuiSkin"</a>
+                <h2>test</h2>
+            </section><!--branding-->
+            <div class="navbar">
+                <ul class="nav navbar-nav">
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="blog.php">Blog</a></li>
+                    <li><a href="about.php">About</a></li>
+
+                </ul>
+            </div><!--navbar-->
+        </header><!--clearfix-->
+    </div><!--column-->
+</div><!--content-->
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-      <meta charset="utf-8">
+        <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>OuiSkin -- Home</title>
@@ -13,6 +13,7 @@
         <section class = "container">
             <div class="content row">
                 <section class = "main col col-lg-8">
+                    <?php include "_/components/php/header.php"; ?>
                 </section><!--main-->
                 <section class = "sidebar col col-lg-4">
                 </section><!--sidebar-->
