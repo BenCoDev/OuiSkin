0e33966989bd04f2044a2c86cfc04e367fa822fe
BenCoDev <benoit.cotte@essec.edu>
2014-04-08 20:12:36 +0200

    header

--- _/components/php/header_left.php
+++
@@ -1,8 +0,0 @@
-<div class="content row">
-    <div class="col-lg-6">
-        <header class="clearfix">
-            <section id="branding">
-                <a href="index.php"><img src="images/misc/logo-OuiSkin_400X185.png" alt="logo for OuiSkin"></a>
-            </section><!--branding-->
-        </header><!--clearfix-->
-    </div><!--column-->
